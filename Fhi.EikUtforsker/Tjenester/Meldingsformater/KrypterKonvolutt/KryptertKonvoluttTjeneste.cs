43fba57c87b376f35393dde1c8a8830fd50824df
Roar Andersen <roar.andersen@fhi.no>
2021-06-04 13:31:26 +0200

    La til bruk av nøkkel-prefiks for KryptertKonvolutt

--- Fhi.EikUtforsker/Tjenester/Meldingsformater/KrypterKonvolutt/KryptertKonvoluttTjeneste.cs
+++ Fhi.EikUtforsker/Tjenester/Meldingsformater/KrypterKonvolutt/KryptertKonvoluttTjeneste.cs
@@ -22,12 +22,20 @@ namespace Fhi.EikUtforsker.Tjenester.Meldingsformater.KrypterKonvolutt
 
         public (string feilmelding, string dekryptert) Dekrypter(string kryptert)
         {
-            var deserialisert = System.Text.Json.JsonSerializer.Deserialize<KryptertRapport>(kryptert);
-            var cipherData = deserialisert?.kryptertKonvolutt?.kryptertObjekt?.cipherData;
-            var keyCipherValue = deserialisert?.kryptertKonvolutt?.kryptertNokkel?.keyCipherValue;
-            var aesKey = DekryptHelper.DekrypterNøkkel(keyCipherValue, _storeName, _storeLocation, _thumbprint);
-            var dataAsString = DekryptHelper.DekrypterBase64Cipher(cipherData, aesKey);
-            return (null, dataAsString);
+            try
+            {
+
+                var deserialisert = System.Text.Json.JsonSerializer.Deserialize<KryptertRapport>(kryptert);
+                var cipherData = deserialisert?.kryptertKonvolutt?.kryptertObjekt?.cipherData;
+                var keyCipherValue = deserialisert?.kryptertKonvolutt?.kryptertNokkel?.keyCipherValue;
+                var aesKey = DekryptHelper.DekrypterLmrEikNøkkel(keyCipherValue, _storeName, _storeLocation, _thumbprint);
+                var dataAsString = DekryptHelper.DekrypterBase64Cipher(cipherData, aesKey);
+                return (null, dataAsString);
+            }
+            catch (Exception ex)
+            {
+                return ("Dekryptering feilet: " + ex.Message, null);
+            }
         }
 
         public List<string> ValiderDekryptertJson(string dekryptert)
